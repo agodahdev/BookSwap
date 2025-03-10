8a60f782716ff411e78876414735a9247ff8d6e5
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:37:00 +0000

    added book model

--- bookswap/books/models.py
+++ bookswap/books/models.py
@@ -1,9 +1,18 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings  # Import settings to access User model
 
-# Custom user model with location field
-class CustomUser(AbstractUser):
-    location = models.CharField(max_length=100, blank=True, null=True)
+# Book model for storing book details
+class Book(models.Model):
+    CONDITION_CHOICES = [('New', 'New'), ('Used', 'Used')]
+    EXCHANGE_TYPE_CHOICES = [('Swap', 'Swap'), ('Giveaway', 'Giveaway')]
+
+    title = models.CharField(max_length=200)  # Book title
+    author = models.CharField(max_length=100)  # Book author
+    genre = models.CharField(max_length=100)  # Book genre
+    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES)  # New/Used
+    exchange_type = models.CharField(max_length=10, choices=EXCHANGE_TYPE_CHOICES)  # Swap or giveaway
+    location = models.CharField(max_length=100)  # Where the book is available
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)  # User who owns the book
 
     def __str__(self):
-        return self.username
+        return self.title  # Return book title as default string

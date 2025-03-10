9b1b79e04b48ebdb54950de8a2b8a9d20ca90746
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 23:35:09 +0000

    Implement book request feature #9

--- bookswap/books/urls.py
+++ bookswap/books/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('add/', add_book, name='add_book'),  # URL for adding books
     path('<int:book_id>/edit/', edit_book, name='edit_book'),
     path('<int:book_id>/delete/', delete_book, name='delete_book'),
+    path('<int:book_id>/request/', request_book, name='request_book'),
 ]

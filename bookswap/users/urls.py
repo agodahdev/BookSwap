71f3f7b8a42324478f800c71812facc412265870
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:30:15 +0000

    added the login/logoutr functionality

--- bookswap/users/urls.py
+++ bookswap/users/urls.py
@@ -3,4 +3,6 @@ from .views import register
 
 urlpatterns = [
     path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]

c1aca062b78c72806dad0e53b907bb1a5a1ff131
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:51:19 +0000

    implement add book feature #5

--- bookswap/books/urls.py
+++ bookswap/books/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import book_list
+from .views import 
+from .views import add_book
 
 urlpatterns = [
     path('', book_list, name='book_list'),  # List all books
+    path('add/', add_book, name='add_book'),  # URL for adding books
 ]

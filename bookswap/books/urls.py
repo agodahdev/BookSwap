4ae238cab161612d5d31a652d6867b33e5ce8686
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 22:04:59 +0000

    Implement delete book feature #7

--- bookswap/books/urls.py
+++ bookswap/books/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', book_list, name='book_list'),  # List all books
     path('add/', add_book, name='add_book'),  # URL for adding books
     path('<int:book_id>/edit/', edit_book, name='edit_book'),
+    path('<int:book_id>/delete/', delete_book, name='delete_book'),
 ]

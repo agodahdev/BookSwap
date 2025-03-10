4eb733ae5602f0760663bc68ed84e12708b1fbc2
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:17:45 +0000

    added user model

--- bookswap/users/models.py
+++ bookswap/users/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+# Custom user model with location field
+class CustomUser(AbstractUser):
+    location = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.username

70ee959de603c7f83457c0c15c71952e4dc457a9
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:49:24 +0000

    added so only logged in users can add books

--- bookswap/books/views.py
+++ bookswap/books/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
 from .models import Book  # Import Book model
+from django.contrib.auth.decorators import login_required
+from .forms import BookForm
 
 # View for listing all books
 def book_list(request):
     books = Book.objects.all()  # Get all books from database
     return render(request, 'books/book_list.html', {'books': books})  # Render template with books
+
+
+# View for adding a book
+@login_required
+def add_book(request):
+    if request.method == 'POST':
+        form = BookForm(request.POST)
+        if form.is_valid():
+            book = form.save(commit=False)
+            book.owner = request.user  # Assign the logged-in user as the book owner
+            book.save()
+            return redirect('book_list')
+    else:
+        form = BookForm()
+    return render(request, 'books/book_form.html', {'form': form})

a36c223f8fe63f90d8aee8eef0a594edb9fd4143
agodahdev Goddah <godahahmed@gmail.com>
2025-03-10 21:42:14 +0000

    added booklist from db

--- bookswap/books/views.py
+++ bookswap/books/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import Book  # Import Book model
 
-# Create your views here.
+# View for listing all books
+def book_list(request):
+    books = Book.objects.all()  # Get all books from database
+    return render(request, 'books/book_list.html', {'books': books})  # Render template with books
